b10ecda805b3557e63b2d901985f99b499b17313
Olivier Mengué <dolmen@cpan.org>
2022-12-20 15:58:59 +0100

    5783, 1: indent SQL

--- 5783/1/1.sql
+++ 5783/1/1.sql
@@ -1,1 +1,25 @@
-select name, phone from customers where name REGEXP REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(phone, '-', ''), '2', '[ABCabc]'), '3', '[DEFdef]'), '4', '[GHIghi]'), '5', '[JKLjkl]'), '6', '[MNOmno]'), '7', '[PQRSpqrs]'), '8', '[TUVtuv]'), '9', '[WXYZwxyz]'), '0', '.'), '1', '.');
+SELECT name, phone
+FROM customers
+WHERE name REGEXP
+	REPLACE(
+		REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(
+						REPLACE(
+							REPLACE(
+								REPLACE(
+									REPLACE(
+										REPLACE(
+											REPLACE(phone, '-', ''),
+											'2', '[ABCabc]'),
+										'3', '[DEFdef]'),
+									'4', '[GHIghi]'),
+								'5', '[JKLjkl]'),
+							'6', '[MNOmno]'),
+						'7', '[PQRSpqrs]'),
+					'8', '[TUVtuv]'),
+				'9', '[WXYZwxyz]'),
+			'0', '.'),
+		'1', '.')
+;
