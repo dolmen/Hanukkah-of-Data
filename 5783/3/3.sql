d25cb428073d9054d8c7bf224f28b58ce54e88a9
Olivier Mengué <dolmen@cpan.org>
2022-12-21 22:25:23 +0100

    5783, 3: add comments

--- 5783/3/3.sql
+++ 5783/3/3.sql
@@ -1,6 +1,9 @@
-SELECT name, phone, birthdate, address
+SELECT name, phone, birthdate, address, citystatezip
 FROM customers
+-- Dog year
 WHERE strftime('%Y', birthdate) % 12 = 2
+-- Aries
 AND strftime('%m-%d', birthdate) BETWEEN '03-21' AND '04-19'
+-- Neighborhood same as JD
 AND citystatezip LIKE '%11420%'
 ;
